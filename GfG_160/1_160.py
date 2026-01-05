4758cb89fbc864f3f0b31134e9aa6f936dd0ab63
Sreehari <srihari770sh@gmail.com>
2026-01-05 19:34:25 +0530

    GFG DAY ONE OF 160

--- GfG_160/1_160.py
+++ GfG_160/1_160.py
@@ -1,1 +1,19 @@
-print("hello")
+class Solution:
+    def getSecondLargest(self, arr):
+        n=len(arr)
+        if n < 2:
+            return -1
+        fst=scd=float('-inf')
+        for num in arr:
+            if num > fst:
+                scd=fst
+                fst=num
+            elif num>scd and num<fst:
+                scd=num
+        if scd==float('-inf'):
+            return -1
+        else:
+            return scd
+a = [12,34,10,35]
+sol = Solution()
+print(sol.getSecondLargest(a))
