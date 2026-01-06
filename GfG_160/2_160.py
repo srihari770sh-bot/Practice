83437d02115199251b317b07801a77850e461387
Sreehari <srihari770sh@gmail.com>
2026-01-06 19:03:11 +0530

    GFG day-2

--- GfG_160/2_160.py
+++ GfG_160/2_160.py
@@ -1,1 +1,8 @@
-print("day 2")
+class Solution:
+    def pushZerosToEnd(self,arr):
+        a = 0
+        for i in range(len(arr)):
+            if arr[i] !=0:
+                arr[i],arr[a] = arr[a],arr[i]
+                a += 1
+        return arr
